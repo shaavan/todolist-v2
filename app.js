36b80114d791dc052000ab89aff328c3482cdfd2
shaavan <shaavan.github@gmail.com>
2023-02-03 16:16:32 +0530

    Completed the todolist app using mongodb backend
    - The user todolist data is now persisted in the database, and can be
      accessed by user anytime.
    - The app now allows user to create any number of custom list, for which
      all the data will be stored in database.

--- app.js
+++ app.js
@@ -1,48 +1,154 @@
-//jshint esversion:6
-
 const express = require("express");
-const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
+
+mongoose.set('strictQuery', false);
+mongoose.connect("mongodb://0.0.0.0:27017/todoListDB");
+
+const itemsSchema = new mongoose.Schema({
+  name: String
+});
+
+const listsSchema = new mongoose.Schema({
+  name: String,
+  list: [itemsSchema]
+});
+
+const Item = mongoose.model("Item", itemsSchema);
+const List = mongoose.model("List", listsSchema)
+
+const item1 = new Item({
+  name: "Buy Food"
+});
+
+const item2 = new Item({
+  name: "Cook Food"
+});
+
+const item3 = new Item({
+  name: "Eat Food"
+});
+
+const defaultItems = [item1, item2, item3];
+
+// Item.insertMany(defaultItems, (err) => {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log("success");
+//   }
+// });
+
+
+
 const workItems = [];
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
 
-const day = date.getDate();
-
-  res.render("list", {listTitle: day, newListItems: items});
-
+  Item.find((err, results) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (results.length === 0) {
+        Item.insertMany(defaultItems, (err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("success");
+          }
+        });
+        res.redirect("/");
+      } else {
+        res.render("list", { listTitle: "Today", newListItems: results });
+      }
+    }
+  });
 });
 
-app.post("/", function(req, res){
+app.post("/", function (req, res) {
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
+  const newItem = new Item({
+    name: itemName
+  });
+
+  if (listName === "Today") {
+    newItem.save().then(() => {
+      res.redirect("/");
+    });
   } else {
-    items.push(item);
-    res.redirect("/");
+    List.findOne({ name: listName }, (err, foundList) => {
+      foundList.list.push(newItem);
+      foundList.save().then(() => {
+        res.redirect("/" + listName);
+      });
+    })
   }
+
+
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+app.post("/delete", function (req, res) {
+  const checkedItemID = req.body.checkbox;
+  const listName = req.body.listName;
+
+  if (listName === "Today") {
+    Item.findByIdAndRemove(checkedItemID, function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Successfully removed the item");
+        res.redirect("/");
+      }
+    });
+  } else {
+    List.findOneAndUpdate({name: listName}, {$pull: {list : {_id: checkedItemID}}}, (err, foundList) => {
+      if(!err) {
+        console.log("Item deleted succesfully");
+        res.redirect("/" + listName);
+      }
+    });
+  }
+
+  
 });
 
-app.get("/about", function(req, res){
+app.get("/:listName", function (req, res) {
+
+  const listName = _.capitalize(req.params.listName);
+
+  List.findOne({ name: listName }, (err, result) => {
+    if (result === null) {
+      // Path creating new list
+      const list = new List({
+        name: listName,
+        list: defaultItems
+      });
+      list.save().then(() => {
+        console.log("No list found, new list created.");
+        res.redirect("/" + listName);
+      });
+    } else {
+      res.render("list", { listTitle: result.name, newListItems: result.list });
+    }
+  })
+
+});
+
+app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
